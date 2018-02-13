f0383200666f6ec75ccfe8192a79b8b4b812d6a8
unknown <matt2uy@gmail.com>
2018-02-12 21:30:49 -0500

    play/preview video

--- scoreboard_overlay.py
+++ scoreboard_overlay.py
@@ -18,9 +18,24 @@ os.system('ffmpeg -version')
 # (triple quotes = the 3rd type of quote)
 os.system('''ffmpeg -i full_match.mp4 -i scoreboard_overlay.png -filter_complex "[0:v][1:v] overlay=25:25:enable='between(t,0,5)'" -pix_fmt yuv420p -c:a copy output.mp4''')
 
-'''
-2. user will traverse video and mark timestamps
+# how do you handle a prompt from cmd? "Overwrite video? type y/n..."
 
+#[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+# wait for process to finish first? search: "os.system wait for process"
+os.system('''ffplay output.mp4''')
+#
+'''
+1.5. make the graphics? (numbers, boxes, etc.)
+
+2. user will traverse video and mark timestamps (is there a way to "preview" the video? is there another ffmpeg video editor to check?)
+	- play video: 
+		- ffplay full_match.mp4
+		- https://ffmpeg.org/ffplay.html
+	- how to use pause/play? other functions?
+	- show the edits made without creating a new file?
+
+	- general wiki: https://www.reddit.com/r/editors/wiki/ffmpeg
+	- more info: https://video.stackexchange.com/questions/tagged/ffmpeg?sort=votes&pageSize=15
 3. somehow show the changes to the user before processing?
 
 4. convert timestamps to ffmpeg commands
